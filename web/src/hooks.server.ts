1ca5eb42959be71e0b29a3044243f50a1e3cc853
Victor Østergaard Nielsen <victordj587@gmail.com>
2026-01-29 12:29:12 +0100

    Update hooks.server.ts

--- web/src/hooks.server.ts
+++ web/src/hooks.server.ts
@@ -5,6 +5,9 @@ import type { Handle } from '@sveltejs/kit';
 import type { ServerInit } from '@sveltejs/kit';
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 import { migrate } from 'drizzle-orm/bun-sqlite/migrator';
+import { Database } from 'bun:sqlite';
+import { DATABASE_URL } from '$env/static/private';
+import * as schema from '$lib/server/db/schema';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	// Fetch current session from Better Auth
@@ -23,7 +26,8 @@ export const init: ServerInit = async () => {
 	if (!dev) {
 		if (process.env.NODE_ENV === 'production') {
 			if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-			const db = drizzle(process.env.DATABASE_URL);
+			const client = new Database(DATABASE_URL, { create: true });
+			const db = drizzle(client, { schema });
 			migrate(db, { migrationsFolder: 'drizzle' });
 			console.log('migrations complete');
 		}
